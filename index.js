789f46f40c053b5d12708cd20dbf45846de1c487
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-04 12:28:25 +0300

    добавив декілька матем.функцій

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ str = camelCase(str);
 
 console.log(str);
 
-const sumArray = (a) => a + a;
-const number = [1, 2, 3, 4, 5];
+function getSum(a, b) {
+  return a * b;
+}
+const resulte = getSum(5, 6);
+console.log('результат', resulte);
+
+let number1 = 15;
+let number2 = 8;
+let sum = number1 + number2;
+console.log(sum);
+
+function newNumbers1() {
+  let numbers1 = [1, 2, 3, 4, 5];
+  return [...numbers1.slice(0, 2), 6, 7, ...numbers1.slice(2)];
+}
+console.log(newNumbers1());
+
+const numbersquared = (a) => a.map((a) => a * a);
+let number = [1, 3, 5, 9, 15];
 console.log(number);
-console.log(sumArray(number));
+console.log(numbersquared(number));
+
+const numbArray = (a) => a.map((a) => a + a);
+let numb = [1, 3, 5, 9, 15];
+console.log(numb);
+console.log(numbArray(numb));
