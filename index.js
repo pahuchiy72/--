4109343d1d096664d3206d98b0b3a2f212588756
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-04 11:40:41 +0300

    add функію +

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ let str = 'Вітаю знову';
 str = camelCase(str);
 
 console.log(str);
+
+const sumArray = (a) => a + a;
+const number = [1, 2, 3, 4, 5];
+console.log(number);
+console.log(sumArray(number));
