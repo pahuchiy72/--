f24fdf68e99de7080b833bea35ac6a49cc45248a
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-06 14:40:35 +0300

    виправив Posts

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+ 
 
 import Counter from './Counter.jsx';
 import Counter1 from './Counter1.jsx';
@@ -14,7 +15,8 @@ import Greeting from './Greeting.jsx';
 import ErrorComponent from './ErrorComponent.jsx';
 import UserProfile from './UserProfile.jsx';
 import User from './User.jsx';
- import TeamList from './TeamList.jsx';
+import TeamList from './TeamList.jsx';
+import Posts from './Posts.jsx';
 
 function Welcome(props) {
   return <h1>Знову Вітаю {props.name} </h1>;
@@ -38,7 +40,6 @@ const teams = [
 function App() {
   return (
     <div>
-      
       <WelcomeComponent user={user} />
       <MyComponent />
       <User user={user} />
@@ -55,6 +56,7 @@ function App() {
       <CounterFunc />
       <Color />
       <TeamList teams={teams} />
+      <Posts />
     </div>
   );
 }
