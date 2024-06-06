89a929280509fc3e75ce691a947049125613dd5c
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-06 14:55:04 +0300

    створили useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Вида. помилку');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+  }, [url]);
+
+  return { data, loading, error };
+}
+export default useFetch;
