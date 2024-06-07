8ae14b2a0e742627cab8a32759c8dd566e6b263d
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-07 14:45:12 +0300

    add Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import UserContext from './context/user';
+
+export default function Footer() {
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>Ваші уподобання</h2>
+      <p>Тема: {user.preferences.theme}</p>
+      <p>Мова користувача: {user.preferences.language}</p>
+    </div>
+  );
+}
