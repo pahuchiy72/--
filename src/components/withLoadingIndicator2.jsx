2228606817d83a5493d2178482f31f6e9fcd1126
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-10 11:08:28 +0300

    add ClockLoader in  withLoadingIndicator2

--- src/components/withLoadingIndicator2.jsx
+++ src/components/withLoadingIndicator2.jsx
@@ -1,14 +1,22 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
+import { ClockLoader } from 'react-spinners';
 
 const withLoadingIndicator2 = (Component) => {
-    return function WithLoadingIndicator2(props) {
-        const [isLoading, setIsLoading] = useState(true);
+  return function WithLoadingIndicator2(props) {
+    const [isLoading, setIsLoading] = useState(true);
 
-        useEffect(()=> {
-            const timer = setTimeout(() => setIsLoading(false), 800);
-            return() => clearTimeout(timer);
-        });
-        return isLoading ? <div>Завантажується трошки почекайте...</div> : <Component {...props} />
-    }
-}
+    useEffect(() => {
+      const timer = setTimeout(() => setIsLoading(false), 800);
+      return () => clearTimeout(timer);
+    });
+    return isLoading ? (
+      <div>
+        Завантажується трошки почекайте...
+        <ClockLoader color="rgba(207, 54, 214, 1)" />
+      </div>
+    ) : (
+      <Component {...props} />
+    );
+  };
+};
 export default withLoadingIndicator2;
