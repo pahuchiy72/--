7d9486b4467e46f740a12d05df8c124fd6a3c2fb
Євген Пахучий <pahuchiy72@ukr.net>
2024-06-10 11:04:24 +0300

    add ClipLoader in JokeFetch

--- src/components/JokeFetch.jsx
+++ src/components/JokeFetch.jsx
@@ -1,11 +1,12 @@
 import useFetch from '../hooks/useFetch';
+import { ClipLoader } from 'react-spinners';
 
 function JokeFetch() {
   const { data, loading, error } = useFetch(
     'https://api.chucknorris.io/jokes/random'
   );
 
-  if (loading) return <p>Зараз завантажуюсь...</p>;
+  if (loading) return <p> <ClipLoader color="#36d7b7" />Зараз завантажуюсь...</p>;
   if (error) return <p> якась помилка: {error.message}</p>;
   return (
     <div>
